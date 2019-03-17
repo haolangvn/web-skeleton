2d2d8f8a6121bd53de6317d7e1f9e023bce3b481
= <=>
2019-03-17 15:22:34 +0700

    update

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii-cms',
+            'dsn' => 'mysql:host=localhost;dbname=web-skeleton_git',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
